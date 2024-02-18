a09bf131862af719a524b5fd2029527898b2500a
MohamedMankouchi <mohamed.mankouchi@student.ehb.be>
2024-02-18 13:53:28 +0100

    Update

--- src/components/Scenario/ScenarioFeedback.jsx
+++ src/components/Scenario/ScenarioFeedback.jsx
@@ -12,11 +12,21 @@ const ExerciseFeedback = ({ step, answer, choice, onComplete }) => {
       <div className="row">
         <div className="col-12 mt-3">
           <p>
+            {console.log(step.step)}
             <strong>Stap {step.step} </strong> : Antwoord: {choice}:{" "}
             {step[`feedback${choice}`]["correct"] ? (
-              <strong>Juist</strong>
+              <strong>Juist </strong>
             ) : (
-              <strong>Fout</strong>
+              <strong>Fout </strong>
+            )}
+            {step[`feedback${choice}`]["correct"] ? (
+              ""
+            ) : step[`feedbackA`]["correct"] ? (
+              <strong> - Juiste antwoord : A</strong>
+            ) : step[`feedbackB`]["correct"] ? (
+              <strong> - Juiste antwoord : B</strong>
+            ) : (
+              <strong> - Juiste antwoord : C</strong>
             )}
           </p>
         </div>
@@ -24,7 +34,7 @@ const ExerciseFeedback = ({ step, answer, choice, onComplete }) => {
       <h2>Feedback</h2>
       <div className="d-flex justify-content-start flex-wrap md-3 mb-5 gap-5 items-center">
         <div className="p-2" style={flexbasis}>
-          <p style={{ width: "450px" }}>A: {step.titleChoiceA}</p>
+          <p style={{ width: "450px" }}></p>
           {step.videoIdChoiceA ? (
             <YoutubeVideo videoId={step.videoIdChoiceA} />
           ) : (
@@ -35,7 +45,7 @@ const ExerciseFeedback = ({ step, answer, choice, onComplete }) => {
           ></div>
         </div>
         <div className="p-2 flex" style={flexbasis}>
-          <p style={{ width: "450px" }}>B: {step.titleChoiceB}</p>
+          <p style={{ width: "450px" }}></p>
           {step.videoIdChoiceB ? (
             <YoutubeVideo videoId={step.videoIdChoiceB} />
           ) : (
@@ -47,7 +57,7 @@ const ExerciseFeedback = ({ step, answer, choice, onComplete }) => {
         </div>
         {step.titleChoiceC ? (
           <div className="p-2 flex" style={flexbasis}>
-            <p style={{ width: "450px" }}>C: {step.titleChoiceC}</p>
+            <p style={{ width: "450px" }}></p>
             {step.videoIdChoiceC ? (
               <YoutubeVideo videoId={step.videoIdChoiceC} />
             ) : (
